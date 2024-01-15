bd199047634c710208554d31bfdd61fa92dd031f
Gaolingx <947770192@qq.com>
2024-01-15 14:53:54 +0800

    20240115_02

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -4,11 +4,24 @@
 #include "stdio.h"
 #include "graphics.h"
 #include "time.h"
+
+#define STATE_UI_MENU 10
+#define STATE_UI_ABOUT 11
+#define STATE_UI_LOADING 12
+#define STATE_UI_GAME 13
+#define STATE_UI_WIN 14
 #define MAX_BOARD 15
 #define MAX_SPACE 50
 TCHAR sc[20];
 IMAGE bk;
 IMAGE zj, zjhb;
+
+TCHAR uic[3][100] = { L"启动！",L"关于游戏",L"退出游戏" };
+
+int uistate = STATE_UI_MENU;
+int uii, uiflag = 0;
+int uilx = 150;
+
 struct Board {
 
 	int x;
@@ -30,7 +43,6 @@ struct Player
 
 int isGameOver = 0;
 int score = 0;
-
 
 void initBoards() {
 	srand((unsigned)time(NULL));
@@ -61,6 +73,98 @@ void drawPlayer()
 	solidcircle(Player.x, Player.y, Player.h);
 
 }
+void drawBoards() {
+	for (int i = 0; i < MAX_BOARD; i++)
+	{
+		setfillcolor(board[i].color);
+		fillrectangle(board[i].x, board[i].y, board[i].x + board[i].length, board[i].y + 5);
+
+	}
+}
+void drawScore() {
+	settextcolor(WHITE);
+	settextstyle(50, 0, L"黑体");
+	wsprintf(sc, L"分数：%d", score);
+	outtextxy(50, 30, sc);
+}
+void drawGame()
+{
+	drawScore();
+	drawBoards();
+	drawPlayer();
+
+}
+void drawGameUI()
+{
+
+	setfillcolor(BLACK);
+	fillrectangle(0, 0, 480, 760);
+
+	switch (uistate)
+	{
+	case STATE_UI_MENU:
+		setcolor(RGB(rand() % 256, rand() % 256, rand() % 256));
+		settextstyle(55, 0, L"宋体");
+		outtextxy(100, 100, L"下 一 百 层");
+		setcolor(GREEN);
+		settextstyle(20, 0, L"宋体");
+		outtextxy(110, 200, L"冷狐无伤版 v1.0.11");
+		settextstyle(32, 0, L"宋体");
+		for (uii = 0; uii < 3; uii++) {
+			outtextxy(200, 250 + uii * 80, uic[uii]);
+		}
+		setfillcolor(BLACK);
+		fillrectangle(160, 255 + uiflag * 80, 190, 285 + uiflag * 80);
+		break;
+
+	case STATE_UI_ABOUT:
+		setcolor(WHITE);
+		settextstyle(25, 0, L"宋体");
+		outtextxy(50, 50, L"测试Text");
+
+		break;
+	case STATE_UI_GAME:
+		setcolor(WHITE);
+		settextstyle(55, 0, L"宋体");
+		drawGame();
+		break;
+	case STATE_UI_WIN:
+
+		setcolor(RED);
+		settextstyle(55, 0, L"黑体");
+		outtextxy(110, 175, L"游戏结束！");
+
+		settextcolor(WHITE);
+		settextstyle(50, 0, L"黑体");
+		wsprintf(sc, L"分数：%d", score);
+		outtextxy(150, 350, sc);
+
+		setcolor(YELLOW);
+		settextstyle(20, 0, L"宋体");
+		outtextxy(75, 500, L"轻触屏幕任意区域重试，按Esc键退出");
+
+		break;
+	case STATE_UI_LOADING:
+		setcolor(WHITE);
+		settextstyle(55, 0, L"宋体");
+		outtextxy(125, 100, L"Loading...");
+		setcolor(WHITE);
+		rectangle(50, 450, 450, 480);
+		setcolor(WHITE);
+		setfillcolor(WHITE);
+		fillrectangle(51, 451, uilx, 479);
+
+		uilx += 5;
+		if (uilx >= 450) {
+			uistate = STATE_UI_GAME;
+			uilx = 150;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
 int inBoard()
 {
 	for (int i = 0; i < MAX_BOARD; i++)
@@ -81,6 +185,7 @@ int inBoard()
 }
 void ctrlGame()
 {
+	/*
 	if (GetAsyncKeyState(VK_LEFT))
 	{
 		Player.x--;
@@ -89,7 +194,65 @@ void ctrlGame()
 	{
 		Player.x++;
 	}
+	*/
+	if (uistate == STATE_UI_MENU || STATE_UI_ABOUT)
+	{
+		//UI控制
+		switch (uistate)
+		{
+		case STATE_UI_MENU:
+			if (GetAsyncKeyState(VK_UP) & 0x8000) {
+				uiflag--;
+				if (uiflag == -1)
+				{
+					uiflag = 2;
+				}
+			}
+			if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+				uiflag = (uiflag + 1) % 3;
+			}
+			if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+				if (uiflag == 0)
+				{
+					uistate = STATE_UI_LOADING;
+				}
+				else if (uiflag == 1)
+				{
+					uistate = STATE_UI_ABOUT;
+				}
+				else
+				{
+					exit(0);
+				}
+			}
+			break;
+		case STATE_UI_ABOUT:
+			if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+				uistate = STATE_UI_MENU;
+			}
+			break;
+		case STATE_UI_GAME:
+			if (GetAsyncKeyState(VK_LEFT))
+			{
+				Player.x--;
+			}
+			if (GetAsyncKeyState(VK_RIGHT))
+			{
+				Player.x++;
+			}
+		case STATE_UI_WIN:
+
+			//TOADD
+			if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
+			{
+				exit(0);
+			}
+		default:
+			break;
+		}
+	}
 }
+
 void movePlayer()
 {
 
@@ -107,25 +270,11 @@ void movePlayer()
 	}
 	if (Player.y > 740 || Player.y < 0) {
 		setfillcolor(BLACK);
-		fillrectangle(0, 0, 480, 760);
-		setcolor(WHITE);
-		settextstyle(55, 0, L"宋体");
-		outtextxy(50, 50, L"Game Over");
-		system("pause");
-
-
+		uistate = STATE_UI_WIN;
 
 	}
 
 
-}
-void drawBoards() {
-	for (int i = 0; i < MAX_BOARD; i++)
-	{
-		setfillcolor(board[i].color);
-		fillrectangle(board[i].x, board[i].y, board[i].x + board[i].length, board[i].y + 5);
-
-	}
 }
 
 void  moveBoards() {
@@ -145,13 +294,16 @@ void  moveBoards() {
 	}
 
 }
-void drawScore() {
-	settextcolor(WHITE);
-	settextstyle(50, 0, L"黑体");
-	wsprintf(sc, L"分数：%d", score);
-	outtextxy(50, 30, sc);
-}
 
+void gameLogic()
+{
+	if (uistate == STATE_UI_GAME)
+	{
+	moveBoards();
+	movePlayer();
+	}
+
+}
 
 int main()
 {
@@ -167,12 +319,10 @@ int main()
 
 
 		cleardevice();
-		drawBoards();
+		drawGameUI();
 		ctrlGame();
-		moveBoards();
-		drawPlayer();
-		movePlayer();
-		drawScore();
+		gameLogic();
+
 		FlushBatchDraw();
 		Sleep(20);
 	}
